84d351dd1d088fa295a044b046d87332aafea398
Dave <55182282+dvsu@users.noreply.github.com>
2026-03-01 21:36:33 +0800

    feat/markdown: Support markdown (#7)
    Support markdown

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,6 +1,7 @@
 import * as json from "./json";
+import * as markdown from "./markdown";
 import * as python from "./python";
 import * as typescript from "./typescript";
 import * as yaml from "./yaml";
 
-export const languages = [json, python, typescript, yaml];
+export const languages = [json, markdown, python, typescript, yaml];
