fa0834bea8bca8b94d2386d5335ad3eeec828a7d
Dave <55182282+dvsu@users.noreply.github.com>
2026-03-01 17:52:20 +0800

    Support JSON

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,4 +1,5 @@
+import * as json from "./json";
 import * as typescript from "./typescript";
 import * as yaml from "./yaml";
 
-export const languages = [typescript, yaml];
+export const languages = [json, typescript, yaml];
